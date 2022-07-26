5878db72c163e35174ebf671dda095862e81b7dc
sripavan-git <pavan862maddela@gmail.com>
2022-07-26 19:37:05 +0530

    try

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app=Flask(__name__)
 
 @app.route("/",methods={"GET","POST"})
 def user():
-    return "<h1> Hello </h1>"
-
+    try:
+        return "<h1> Hello </h1>"
+    except Exception as e:
+        print(str(e))
 if __name__=="__main__":
     app.run(debug=True)
