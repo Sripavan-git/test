5097fe820901a196ba24acc213ae2edca95c5de5
sripavan-git <pavan862maddela@gmail.com>
2022-07-26 19:46:33 +0530

    added pages

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask 
+from flask import Flask ,render_template
 
 app=Flask(__name__)
 
 @app.route("/",methods={"GET","POST"})
 def user():
     try:
-        return "<h1> Hello </h1>"
+        return render_template("index.html")
     except Exception as e:
         return e 
+
+
+@app.route("/test")
+def test():
+    try:
+        return "helloo "
+    except Exception as e:
+        return "error caught: "+e
+
 if __name__=="__main__":
     app.run(debug=True)
